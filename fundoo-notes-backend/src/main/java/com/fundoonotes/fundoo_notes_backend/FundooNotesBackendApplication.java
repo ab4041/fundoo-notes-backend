cfc31589398669bfbc3d2d362898b1ca5135686b
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-04-20 11:54:48 +0530

    added application.properties config

---
+++ fundoo-notes-backend/src/main/java/com/fundoonotes/fundoo_notes_backend/FundooNotesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fundoonotes.fundoo_notes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundooNotesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundooNotesBackendApplication.class, args);
+	}
+
+}
---
+++ fundoo-notes-backend/src/test/java/com/fundoonotes/fundoo_notes_backend/FundooNotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundoonotes.fundoo_notes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundooNotesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
