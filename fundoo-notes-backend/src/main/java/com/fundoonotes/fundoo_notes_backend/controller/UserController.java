5d7981d37c1d31f125a96ae7c4771b63d94f6347
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-04-20 12:14:29 +0530

    added user controller

---
+++ fundoo-notes-backend/src/main/java/com/fundoonotes/fundoo_notes_backend/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.fundoonotes.fundoo_notes_backend.controller;
+
+import com.fundoonotes.fundoo_notes_backend.dto.request.UserRegisterRequestDto;
+import com.fundoonotes.fundoo_notes_backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public String register(
+            @RequestBody UserRegisterRequestDto dto
+    ) {
+        return userService.register(dto);
+    }
+}
