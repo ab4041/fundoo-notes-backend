4626470b034be54b683f3344d91ab2c801643f33
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-04-20 14:30:02 +0530

    added login api endpoint

--- fundoo-notes-backend/src/main/java/com/fundoonotes/fundoo_notes_backend/controller/UserController.java
+++ fundoo-notes-backend/src/main/java/com/fundoonotes/fundoo_notes_backend/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.fundoonotes.fundoo_notes_backend.controller;
 
+import com.fundoonotes.fundoo_notes_backend.dto.request.LoginRequestDto;
 import com.fundoonotes.fundoo_notes_backend.dto.request.UserRegisterRequestDto;
+import com.fundoonotes.fundoo_notes_backend.dto.response.LoginResponseDto;
 import com.fundoonotes.fundoo_notes_backend.service.UserService;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,10 +16,26 @@ public class UserController {
 
     private final UserService userService;
 
+
+    // REGISTER API
     @PostMapping("/register")
     public String register(
             @RequestBody UserRegisterRequestDto dto
     ) {
+
         return userService.register(dto);
+
     }
+
+
+    // LOGIN API
+    @PostMapping("/login")
+    public LoginResponseDto login(
+            @RequestBody LoginRequestDto dto
+    ) {
+
+        return userService.login(dto);
+
+    }
+
 }
